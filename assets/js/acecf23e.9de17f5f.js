07eddcc5d4f3f2662cce2adeab0e86543cfa8f91
2Naq <2Naq@users.noreply.github.com>
2026-06-21 09:59:22 +0000

    deploy: 928e3270eea45e197715b94821171404ce67e3c5

---
+++ assets/js/acecf23e.9de17f5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshareme=self.webpackChunkshareme||[]).push([["3220"],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/shareme/blog","blogTitle":"Blog","authorsListPath":"/shareme/blog/authors"}')}}]);
