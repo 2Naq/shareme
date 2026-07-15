8359abe8e224611630dc9101729cad72fc0a859e
anDev <con125423@gmail.com>
2026-07-15 16:37:30 +0700

    add favicon docs + tool

--- apps/docs/src/theme/Navbar/index.js
+++ apps/docs/src/theme/Navbar/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import Navbar from '@theme-original/Navbar';
+import clsx from 'clsx';
 
 export default function NavbarWrapper(props) {
   return (
     <>
-      <style>{`
-               .navbar-glass {
-                 background-color: rgba(255, 255, 255, 0.6) !important;
-                 backdrop-filter: blur(12px) saturate(180%);
-                 -webkit-backdrop-filter: blur(12px) saturate(180%);
-                 border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-                 box-shadow: 0 4px 30px rgba(0, 0, 0, 0.05) !important;
-               }
-               html[data-theme='dark'] .navbar-glass {
-                 background-color: rgba(10, 10, 10, 0.5) !important;
-                 border-bottom: 1px solid rgba(255, 255, 255, 0.05);
-               }
-             `}</style>
-      <Navbar className="navbar-glass" {...props} />
+      <Navbar {...props} className={clsx('navbar-glass', props.className)} />
     </>
   );
 }
