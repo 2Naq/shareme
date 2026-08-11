0315cd0402d8534349d9564958d240e54402baa1
2Naq <2Naq@users.noreply.github.com>
2026-08-11 18:18:15 +0000

    deploy: d9bc5ad10bcbf312a055b344f766741497994c19

--- tools/sw.js
+++ tools/sw.js
@@ -10,13 +10,17 @@ const ASSETS_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-      .then(() => self.skipWaiting()),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }),
   );
+});
+
+// Listen for SKIP_WAITING message from the client (user clicked "Update now")
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener("activate", (event) => {
